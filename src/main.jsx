6abd96dd0daee9b4a10cade359fbc0585cc649bd
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-25 10:54:00 +0530

    feat: created pages and added frontend routes using createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
+import ProductListing from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Cart from './pages/Cart.jsx'
+const router=createBrowserRouter([{
+  path:"/",
+  element:<ProductListing/>
+},{
+  path:"/cart",
+  element:<Cart/>
+}])
+function AppRender(){
+  return(<StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>)
+}
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AppRender/>
 )
